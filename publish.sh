624fdef4e4767ef2ad2875b91065c6d0c697455c
Rob Thijssen <rthijssen@gmail.com>
2015-07-15 12:07:16 +0100

    remove deprecated files on publish

--- publish.sh
+++ publish.sh
@@ -6,6 +6,6 @@ cp dist pages_repo/bugzoolla
 cd pages_repo
 git config user.name "${GIT_NAME}"
 git config user.email "${GIT_EMAIL}"
-git add bugzoolla
+git add bugzoolla -A
 git commit -m "Travis CI publish"
 git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
