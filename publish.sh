f723c17755b2c95c5d99d11a65ae719038577878
Rob Thijssen <rthijssen@gmail.com>
2015-07-15 11:51:43 +0100

    travis publish

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone "https://${GH_TOKEN}@${GH_REF}" pages_repo
+rm -rf pages_repo/bugzoolla
+cp dist pages_repo/bugzoolla
+cd pages_repo
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+git add bugzoolla
+git commit -m "Travis CI publish"
+git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
