149f97945a466355d95b0c6c2f05e6eac3d350b8
Rob Thijssen <rthijssen@gmail.com>
2015-12-15 12:11:45 +0000

    blockers and dependencies

--- app/scripts/controllers/bug.js
+++ app/scripts/controllers/bug.js
@@ -14,5 +14,21 @@ angular.module('bugzoollaApp')
       CommentFactory.get({ bug_id: $routeParams.id }, function(data) {
         $scope.bug.comments = data.bugs[$routeParams.id].comments;
       });
+      $scope.bug.depended_on_bugs = [];
+      if ($scope.bug.depends_on) {
+        $scope.bug.depends_on.forEach(function(depended_on_bug_id) {
+          BugFactory.get({ id: depended_on_bug_id }, function(depended_on_bug_data) {
+            $scope.bug.depended_on_bugs.push(depended_on_bug_data.bugs[0]);
+          });
+        });
+      }
+      $scope.bug.blocked_bugs = [];
+      if ($scope.bug.blocks) {
+        $scope.bug.blocks.forEach(function(blocked_bug_id) {
+          BugFactory.get({ id: blocked_bug_id }, function(blocked_bug_data) {
+            $scope.bug.blocked_bugs.push(blocked_bug_data.bugs[0]);
+          });
+        });
+      }
     });
   });
